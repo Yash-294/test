b9c5cefd26aa83e2ae73b650452729f24947a525
Yash Shinde <yash.shinde@xecurify.com>
2024-09-20 14:40:46 +0530

    Second Commit

--- miniorange/build.gradle.kts
+++ miniorange/build.gradle.kts
@@ -4,12 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.andorid.miniorange"
+    namespace = "com.android.miniorange"
     compileSdk = 34
 
     defaultConfig {
         minSdk = 24
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -30,12 +29,36 @@ android {
     }
 }
 
+publishing {
+    publications {
+        // Java Publication for API dependencies
+        create<MavenPublication>("mavenJava") {
+            pom.withXml {
+                val dependenciesNode = asNode().appendNode("dependencies")
+                configurations["api"].allDependencies.forEach { dependency ->
+                    val dependencyNode = dependenciesNode.appendNode("dependency")
+                    dependencyNode.appendNode("groupId", dependency.group)
+                    dependencyNode.appendNode("artifactId", dependency.name)
+                    dependencyNode.appendNode("version", dependency.version)
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri("https://jitpack.io") // Your repository information goes here
+        }
+    }
+}
+
+
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
-dependencies {
 
+dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     testImplementation("junit:junit:4.13.2")
@@ -43,5 +66,4 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     api("com.android.volley:volley:1.2.1")
     api("org.bouncycastle:bcprov-jdk15on:1.70")
-
 }
